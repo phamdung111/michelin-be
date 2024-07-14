c948b786325068da123027205c9453211f15b75f
phamdung111 <88172121+phamdung111@users.noreply.github.com>
2024-07-15 05:13:38 +0700

    Api Admin Restaurant (#3)

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class AdminController extends Controller
+{
+    public function restaurants(Request $request){
+        $page = $request->get('page');
+        try{
+            $restaurants = Restaurant::
+                with(['images'])
+                ->orderByDesc('created_at')
+                ->paginate(8, ['*'], 'page',(int) $page);
+            if ($restaurants->isEmpty()) {
+                return response()->json(['message' => 'No restaurants found'], 200);
+            }
+            $restaurantsData = $restaurants->map(function ($restaurant) {
+                return [
+                    'id'=> $restaurant->id,
+                    'name' => $restaurant->name,
+                    'status'=> $restaurant->status,
+                    'address' => $restaurant->address,
+                    'phone'=> $restaurant->phone,
+                    'email' => $restaurant->email,
+                    'description'=> $restaurant->description,
+                    'allow_booking'=>(bool) $restaurant->allow_booking,
+                    'date' => date('H:i d/m/Y', strtotime($restaurant->created_at)),
+                    'images' => $restaurant->images->map(function ($image) {
+                        return [
+                            'id' => $image->id,
+                            'image' => Storage::url($image->image)
+                        ];
+                    })->toArray()
+                ];
+            });
+            
+            return response()->json([
+                'items' => $restaurantsData,
+                'current_page' => $restaurants->currentPage(),
+                'per_page' => $restaurants->perPage(),
+                'total' => $restaurants->total(),
+                'last_page' => $restaurants->lastPage(),
+            ],200);
+        }catch(\Exception $e) {
+            return response()->json(['error'=> $e->getMessage()], 400);
+        }
+    }
+    public function permissionRestaurants(Request $request){
+        $data = $request->all();
+
+        try{
+            foreach ($data as $restaurant_id => $restaurant_status) {
+                
+                $restaurant = Restaurant::find($restaurant_id);
+                $restaurant->status = $restaurant_status;
+                $restaurant->save();
+            }
+            return response()->json(['status'=> 'success'],200);
+        }catch(\Exception $e) {
+            return response()->json(['error'=> $e->getMessage()], 400);
+        }
+        
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,10 +8,20 @@ use Illuminate\Http\Request;
  
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        using: function () {
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+        
+        Route::middleware('api')
+            ->prefix('admin')
+            ->group(base_path('routes/admin.php'));
+ 
+ 
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
