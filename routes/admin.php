646ae113e18b9e4a1cc84464ff51f3ba2cc2bca6
phamdung111 <dungvanx8@gmail.com>
2024-07-15 05:01:46 +0700

    Api Admin Restaurant

---
+++ routes/admin.php
@@ -0,0 +1,14 @@
+<?php
+ 
+use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Route;
+ 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => ''
+], function ($router) {
+    Route::post('/permission-restaurants', [AdminController::class, 'permissionRestaurants'])->middleware('auth:api')->name('me');
+});
+
+
+Route::get('/restaurants', [AdminController::class, 'restaurants'])->middleware('auth:api')->name('me');
